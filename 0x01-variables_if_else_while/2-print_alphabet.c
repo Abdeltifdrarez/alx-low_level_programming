a52f74046e0e9faf371d33862c149a907777f836
Abdeltifdrarez <drarezabdeltif@gmail.com>
2023-03-17 10:41:13 -0700

    Print alphapet lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,11 @@
  * number stored in the variable n
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char ch;
-	for (ch = 'a';ch <= 'z'; ch++)
+
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
