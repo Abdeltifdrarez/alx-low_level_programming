b752c9e8dd022b6fde9dbec9e6e09b6461d90fab
Abdeltifdrarez <drarezabdeltif@gmail.com>
2023-03-17 11:10:32 -0700

    Prints all single figit numbers of base 1à startinfg from 0.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	       printf("%d", num);
+	printf("\n"):
+	return (0):
+}	
